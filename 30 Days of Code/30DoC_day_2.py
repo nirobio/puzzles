126640fbe4aecce2502e62c559a880518ee28306
niromics <52195521+Khimaera@users.noreply.github.com>
2021-01-06 08:32:39 +0000

    30DoC day 1

---
+++ 30 Days of Code/30DoC_day_2.py
@@ -0,0 +1,17 @@
+first = int(input())
+second = float(input())
+third = str(input())
+
+print(int(first + i))
+print(float(second + d))
+print((s) + (third))
+
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
