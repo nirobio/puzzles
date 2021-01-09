bba81a882bcc8fc7fe09ed32fe96f3f6f5120e41
niromics <52195521+Khimaera@users.noreply.github.com>
2021-01-09 11:29:20 +0000

    Create 30DoC_day_6.py

---
+++ 30 Days of Code/30DoC_day_6.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    new_arr = map(str, arr[::-1])
+    print(" ".join(new_arr))
