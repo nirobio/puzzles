a9faeca315c07899c3efb22178563daaae1f3177
niromics <52195521+Khimaera@users.noreply.github.com>
2021-01-12 10:07:18 +0000

    Create 30DoC_Day_9.py

---
+++ 30 Days of Code/30DoC_Day_9.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+# factorial function using a recursive method
+# factorial(n) = 1/(n * factorial(n-1))
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
