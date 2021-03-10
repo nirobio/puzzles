335d1a06cbdf945aa49c34166349d2d9dbcdfba6
Niro-y <52195521+Khimaera@users.noreply.github.com>
2021-03-10 20:36:05 +0000

    Create capitalize.py

---
+++ capitalize.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def solve(s):
+    #' '.join(map(str.capitalize, s.split(' ')))
+    for x in s[:].split():
+        s = s.replace(x, x.capitalize())
+    return s
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
