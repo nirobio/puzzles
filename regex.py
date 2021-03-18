bc79ae678e788a47a5edaf2833b44e4b96506979
Niro-y <52195521+Khimaera@users.noreply.github.com>
2021-03-18 22:57:47 +0000

    regular expressions

---
+++ regex.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/introduction-to-regex/problem
+import re
+
+class Main():
+    def __init__(self):
+        self.n = int(input())
+
+        for i in range(self.n):
+            self.s = input()
+            print(bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$', self.s)))
+
+if __name__ == '__main__':
+    obj = Main()
