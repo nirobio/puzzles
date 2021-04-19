8027a0963b1b9488cbcb3d6098ea7fefd281656a
Niro-y <52195521+Khimaera@users.noreply.github.com>
2021-04-19 11:02:39 +0100

    Create itertools_combinations.py

---
+++ itertools_combinations.py
@@ -0,0 +1,6 @@
+from itertools import combinations
+
+s = input().split()
+string, number = sorted(s[0]), int(s[1])
+for i in range(1, number + 1):
+    print(*list(map(''.join, combinations(string, i))), sep='\n')
